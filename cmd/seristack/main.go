6db726fc52db6eebabf0cac0dc3ef0b7f1d128c1
sudharsane sivamany <sudharsane.sivamany@oracle.com>
2026-05-25 00:48:50 +0530

    added

--- cmd/seristack/main.go
+++ cmd/seristack/main.go
@@ -27,7 +27,7 @@ Visit https://seristack.getsaas.in/ for more information.
 See our work on https://github.com/TechXploreLabs/seristack.
 
 Apache 2.0 License.`,
-	Version: "0.3.1",
+	Version: "0.3.2",
 }
 
 func Execute() {
